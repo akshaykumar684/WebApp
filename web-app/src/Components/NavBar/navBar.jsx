ca5d9dab008e896bdc916944ef5c86d943580c93
Akshay Kumar Mallick <PM9B@eurofins.com>
2020-11-04 18:26:59 +0530

    to Warning error removed from Link tag

--- web-app/src/Components/NavBar/navBar.jsx
+++ web-app/src/Components/NavBar/navBar.jsx
@@ -9,7 +9,7 @@ class NavBar extends Component {
         const { search } = this.state;
         return (
             <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-                <Link className="navbar-brand">Akshay</Link>
+                <Link to="home" className="navbar-brand">Akshay</Link>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
                     aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
