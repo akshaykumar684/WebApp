6455dc73100d74c08cc20d3d70546efe59c86c71
akshaykumar684 <akshaykrkat@gmail.com>
2020-12-19 23:59:09 +0530

    testCommit

--- web-app/src/Components/TestComponent/TestComponent.jsx
+++ web-app/src/Components/TestComponent/TestComponent.jsx
@@ -1,35 +1,35 @@
 import React, { Component } from "react";
 
 class Test extends Component {
-
-    state = {
-        loading : true,
-        users : []
-    };
-    componentDidMount()
-    {
-        console.log("Inside Component Did mount");
-        fetch("https://jsonplaceholder.typicode.com/users")
-          .then((response) => response.json())
-          .then((json) => {
-            console.log(json);
-            this.setState({
-                loading : false,
-                users : json
-            })
-          });
-    }
+  state = {
+    loading: true,
+    users: [],
+  };
+  componentDidMount() {
+    console.log("Inside Component Did mount");
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((json) => {
+        console.log(json);
+        this.setState({
+          loading: false,
+          users: json,
+        });
+      });
+  }
   render() {
     return (
-     <div>
-         {this.state.loading?<p>loading</p>:
-         <ul>
-             {this.state.users.map(user => (
-                 <li key={user.id}>{user.name}</li>
-             ))}
-         </ul>
-         }
-     </div>
+      <div>
+        {this.state.loading ? (
+          <p>loading</p>
+        ) : (
+          <ul>
+            {this.state.users.map((user) => (
+              <li key={user.id}>{user.name}</li>
+            ))}
+          </ul>
+        )}
+      </div>
     );
   }
 }
